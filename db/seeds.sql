13178fe77bbf05c31fec86cfe2220a9316e2ca4e
Ian Hooper <ianhooper8@gmail.com>
2020-05-14 01:46:36 -0400

    adding schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Spicy Burger', true);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Double Burger', false);
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Turkey Burger', true);
